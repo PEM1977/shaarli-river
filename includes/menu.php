09cf7565e160e4d7dcc544e87682bff23b426a6d
ArthurHoaro <arthur@hoa.ro>
2015-04-12 12:34:57 +0200

    Fix broken TV link

--- includes/menu.php
+++ includes/menu.php
@@ -3,7 +3,7 @@
 	<a id="link-river" class="btn btn-default" href="./index.php"><span class="glyphicon glyphicon-signal"></span> The River</a>
 	<a id="link-top" class="btn btn-default" href="./top.php"><span class="glyphicon glyphicon-star"></span> Top Shared Links</a>
 	<a id="link-search" class="btn btn-default" href="./search.php"><span class="glyphicon glyphicon-search"></span> Search</a>
-	<a class="btn btn-default" target="_blank" href="https://nexen.mkdir.fr/shaarli-tv/"><span class="glyphicon glyphicon-film"></span> TV</a>
+	<a class="btn btn-default" target="_blank" href="https://river.hoa.ro/tv/"><span class="glyphicon glyphicon-film"></span> TV</a>
 	<a class="btn btn-default" target="_blank" href="https://shaarlimages.net/"><span class="glyphicon glyphicon-picture"></span> Pictures</a>
 	<a class="btn btn-default" target="_blank" href="https://shaarli.fr/"><span class="glyphicon glyphicon-retweet"></span> Discussions</a>
 	<a class="btn btn-default" target="_blank" href="https://shaarli.fr/jappix/?r=shaarli@conference.dukgo.com"><span class="glyphicon glyphicon-comment"></span> Chat</a>		
